bc2dd963c9a96e1d59dcb9208f21e39a2c13bcc2
akash07marwah <akashmarwah07@gmail.com>
2019-03-23 15:27:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const electron = require('electron');
+// Module to control Application life
+const app = electron.app;
+const url = require('url');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow()
+{
+    mainWindow = new electron.BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    
+    mainWindow.loadURL(url.format({
+     pathname: path.join(__dirname, 'index.html'),
+     protocol: 'file:',
+     slashes:true
+
+ }));
+
+mainWindow.webContents.openDevTools();
+
+mainWindow.on('closed',() =>{
+    mainWindow=null;
+} );
+}
+app.on('ready',createWindow); 
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+if(mainWindow === null){
+    createWindow();
+}
+});
